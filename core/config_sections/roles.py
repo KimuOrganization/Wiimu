9397aafd7606cc275c789d407367361247e64813
LautaroCepeda-Developer <lautarocepeda.developer@gmail.com>
2026-08-03 00:15:51 -0300

    feat: Add IntegrationRoles class for managing integration role IDs

--- core/config_sections/roles.py
+++ core/config_sections/roles.py
@@ -18,8 +18,20 @@ class CommonRoles(ConfigSection):
     def __getattr__(self, name:str) -> int:
         return self._get(name)
 
+class IntegrationRoles(ConfigSection):
+    """IDs de los roles de integración (ej: miembros yt, miembros kofi, boosters, etc). Tener en cuenta que generalmente van a ser listas de IDs por el hecho de que una integración puede tener mas de un rol."""
+    PREFIX = "ROLES_ID.INTEGRATION"
+
+    KOFI_MEMBER: list[int]
+    SERVER_BOOSTER: list[int]
+    YOUTUBE_MEMBER: list[int]
+
+    def __getattr__(self, name:str) -> int:
+        return self._get(name)
+
 class Roles:
     """Clase para acceder a las IDs de los roles registrados en la DB"""
     def __init__(self, manager) -> None:
         self.staff = StaffRoles(manager)
         self.common = CommonRoles(manager)
+        self.integration = IntegrationRoles(manager)
